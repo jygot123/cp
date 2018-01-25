ec36b1bde92e3a38cf6a9ebc772917fefdb2a69e
Arifmaulana <arifmaulananp13@gmail.com>
2018-01-25 15:15:00 +0700

    setting up template into CI

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_index extends CI_Controller {
+class Home extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
+		$this->load->helper('url');
 	}
 
 	public function index(){
 $this->load->view('v_cp');
 		}
-	
 }
--- application/views/v_cp.php
+++ application/views/v_cp.php
@@ -1,4 +1,155 @@
-            <div id="subMenuJs"></div>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+    <title>Sudzuki - One Page Bootstrap Template</title>
+    <meta content="xenia - responsive and retina ready template" name="description">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport"/>
+    <link href="<?=base_url('assets/images/favicon.ico');?>" rel="shortcut icon"/>
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/apple-touch-icon-144x144-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/apple-touch-icon-114x114-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/apple-touch-icon-72x72-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" href="assets/images/apple-touch-icon-precomposed.png" />
+    <!-- JS FILES -->
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery-1.20.2.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery-migrate-1.2.1.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/rs-plugin/js/jquery.themepunch.plugins.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/rs-plugin/js/jquery.themepunch.revolution.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/modernizr.custom.js');?>"></script>
+    <!-- CSS FILES -->
+    <link href="<?=base_url('assets/rs-plugin/css/settings.css');?>" media="screen" rel="stylesheet" type="text/css" >
+    <link href="<?=base_url('assets/css/navstylechange.css');?>" media="screen" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('assets/css/flexslider.css');?>" media="screen" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('assets/css/cubeportfolio.min.css');?>" media="screen" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('assets/css/jquery.bxslider.css');?>" media="screen" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('assets/css/style.css');?>" media="screen" rel="stylesheet" type="text/css">
+    <link href="<?=base_url('assets/css/responsive.css');?>" media="screen" rel="stylesheet" type="text/css">
+  </head>
+  <body class="loading">
+    <div id="wrapper">
+        <div class="tp-banner-container home" style="height:1000px;" id="1">
+            <div class="tp-banner">
+                <ul style="display:none;">
+                    <li data-transition="fade" data-slotamount="5" data-masterspeed="700">
+                        <img src="<?=base_url('assets/images/bg1.jpg');?>"   alt=""  data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
+                        <div class="tp-caption largewhite sft"
+                            data-x="center"
+                            data-y="140"
+                            data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                            data-speed="500"
+                            data-start="800"
+                            data-easing="Back.easeOut"
+                            data-endspeed="500"
+                            data-endeasing="Power4.easeIn"
+                            data-captionhidden="on"
+                            style="z-index: 4">Accelerate your business
+                        </div>
+                        <div class="tp-caption mediumwhite sfb"
+                            data-x="center"
+                            data-y="240"
+                            data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                            data-speed="500"
+                            data-start="1200"
+                            data-easing="Back.easeOut"
+                            data-endspeed="500"
+                            data-endeasing="Power4.easeIn"
+                            data-captionhidden="on"
+                            style="z-index: 4">With our perfect solution
+                        </div>
+                        <div class="tp-caption mediumbutton lft"
+                            data-x="center"
+                            data-y="340"
+                            data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                            data-speed="500"
+                            data-start="1600"
+                            data-easing="Back.easeOut"
+                            data-endspeed="500"
+                            data-endeasing="Power4.easeIn"
+                            data-captionhidden="on"
+                            style="z-index: 4"><a href="#" class="service-link">Get started</a>
+                        </div>
+                   </li>
+                    <li data-transition="random" data-slotamount="5" data-masterspeed="700" >
+                        <img src="assets/images/bg2.jpg"   alt=""  data-bgfit="cover" data-bgposition="center center" data-bgrepeat="no-repeat">
+                        <div class="tp-caption bglargewhite sft"
+                            data-x="center"
+                            data-y="100"
+                            data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                            data-speed="500"
+                            data-start="800"
+                            data-easing="Back.easeOut"
+                            data-endspeed="500"
+                            data-endeasing="Power4.easeIn"
+                            data-captionhidden="on"
+                            style="z-index: 4">Sudzuki is the <span>fastest template</span>
+                        </div>
+                        <div class="tp-caption bgmediumwhite sfb"
+                            data-x="center"
+                            data-y="210"
+                            data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                            data-speed="500"
+                            data-start="1200"
+                            data-easing="Back.easeOut"
+                            data-endspeed="500"
+                            data-endeasing="Power4.easeIn"
+                            data-captionhidden="on"
+                            style="z-index: 4">With tons of features
+                        </div>
+                        <div class="tp-caption mediumbutton lft"
+                            data-x="center"
+                            data-y="320"
+                            data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                            data-speed="500"
+                            data-start="1600"
+                            data-easing="Back.easeOut"
+                            data-endspeed="500"
+                            data-endeasing="Power4.easeIn"
+                            data-captionhidden="on"
+                            style="z-index: 4"><a href="#" class="service-link">Get started</a>
+                        </div>
+                   </li>
+                </ul>
+                <div class="tp-bannertimer"></div>
+            </div>
+        </div>
+                <script type="text/javascript">
+                var revapi;
+                jQuery(document).ready(function() {
+                      revapi = jQuery('.tp-banner').revolution(
+                        {
+                            delay:9000,
+                            startwidth:1170,
+                            startheight:500,
+                            hideThumbs:10,
+                            fullWidth:"on",
+                            fullScreen:"on",
+                            fullScreenOffsetContainer: ""
+                        });
+                }); //ready
+            </script>
+            <div class="subMenu">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-3 pull-left"><div class="logo"><img src="<?=base_url('assets/images/logo.png');?>" alt=""></div></div>
+                        <div class="col-lg-9 col-md-9 col-sm-9 pull-right">
+                            <div class="inner">
+                                <div id="dl-menu" class="dl-menuwrapper"><button class="dl-trigger">Open Menu</button>
+                                    <ul class="dl-menu">
+                                        <li><a href="#" id="home" class="subNavBtn">Home</a></li>
+                                        <li><a href="#" id="service" class="subNavBtn">Services</a></li>
+                                        <li><a href="#" id="portfolio" class="subNavBtn">Portfolio</a></li>
+                                        <li><a href="#" id="about" class="subNavBtn">About Us</a></li>
+                                        <li><a href="#" id="blog" class="subNavBtn">Blog</a></li>
+                                        <li><a href="#" id="contact" class="subNavBtn">Contact</a></li>
+                                    </ul>
+                                </div>                                 
+                            </div> 
+                        </div> 
+                    </div>
+                </div>        
+            </div>
+			<div id="subMenuJs"></div>
             <div class="serv-bg service" id="2">
                 <div class="container" id="service-scroll">
                     <div class="row marg75">
@@ -12,32 +163,78 @@
                             <div class="serv-3 serv-6 serv-12">
                                 <div class="serv-block sb1">
                                     <div class="serv-icon s1"><i class="icon-rocket"></i></div>
-                                    <div class="serv-name">Easy to launch</div>
-                                    <div class="serv-desc">This template was designed specially  for you. It’s easy to customize anything. Fully responsive layout</div>
+                                    <div class="serv-name">iOS Development</div>
+                                    <div class="serv-desc">Our team develop iOS apps to accommodate clients requirements.</div>
                                 </div>
                             </div>
                             <div class="serv-3 serv-6 serv-12">
                                 <div class="serv-block sb2">
                                     <div class="serv-icon s2"><i class="icon-speedometer"></i></div>
-                                    <div class="serv-name">SEO optimized</div>
-                                    <div class="serv-desc">This template was designed specially  for you. It’s easy to customize anything. Fully responsive layout</div>
+                                    <div class="serv-name">Android Development</div>
+                                    <div class="serv-desc">We offer customized android apps development based on your requirements and needs.</div>
                                 </div>
-                            </div>
+							</div>
                             <div class="serv-3 serv-6 serv-12">
                                 <div class="serv-block sb3">
                                     <div class="serv-icon s3"><i class="icon-diamond"></i></div>
-                                    <div class="serv-name">Unique design</div>
-                                    <div class="serv-desc">This template was designed specially  for you. It’s easy to customize anything. Fully responsive layout</div>
+                                    <div class="serv-name">Web Development</div>
+                                    <div class="serv-desc">We provide state of the art web design and powerful web application services with new platform and technology.</div>
                                 </div>
                             </div>
                             <div class="serv-3 serv-6 serv-12">
                                 <div class="serv-block sb4">
                                     <div class="serv-icon s4"><i class="icon-screen-tablet"></i></div>
-                                    <div class="serv-name">Fully responsive</div>
-                                    <div class="serv-desc">This template was designed specially  for you. It’s easy to customize anything. Fully responsive layout</div>
+                                    <div class="serv-name">Desktop Development</div>
+                                    <div class="serv-desc">We solve issues and problems by advanced desktop application platform</div>
+                                </div>
+                            </div>
+							<div class="serv-3 serv-6 serv-12">
+                                <div class="serv-block sb4">
+                                    <div class="serv-icon s4"><i class="icon-screen-tablet"></i></div>
+                                    <div class="serv-name">IT Resource Solution</div>
+                                    <div class="serv-desc">We provide IT Experts to meet your resources needs. Focused areas such as IT software development, Technical writer, and Project Management.</div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+			  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.slider4').bxSlider({
+        slideWidth: 200,
+        minSlides: 1,
+        maxSlides: 5,
+        moveSlides: 1,
+        slideMargin: 35,
+        auto: true,
+        pause: 4000,
+        speed: 1000,
+        controls:false,
+        pager:false
+      });
+    });
+    </script>
+    <script type="text/javascript" src="<?=base_url('assets/js/bootstrap.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery.cubeportfolio.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/portfolio.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/retina.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/contacts.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery.bxslider.min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery.dlmenu.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery.flexslider-min.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery.parallax-1.1.3.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/main.js');?>"></script>
+    <script type="text/javascript" src="<?=base_url('assets/js/jquery.smint.js');?>"></script>
+    <script type="text/javascript">
+    $(document).ready( function() {
+        var sliderApi = jQuery('.tp-banner').revolution();
+        sliderApi.bind("revolution.slide.onloaded",function (e) {
+            $('.subMenu').smint({
+                'scrollSpeed' : 1000
+            });
+        });
+    });
+    </script>
+  </body>
+</html>
